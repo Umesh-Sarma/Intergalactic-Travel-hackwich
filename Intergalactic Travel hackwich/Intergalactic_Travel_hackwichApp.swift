8cb4667bdfa207b1a4c4ddafd4e8596d837d2621
Umesh Sarma <usarma1200@gmail.com>
2023-10-24 09:31:39 -0500

    Initial Commit

---
+++ Intergalactic Travel hackwich/Intergalactic_Travel_hackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Intergalactic_Travel_hackwichApp.swift
+//  Intergalactic Travel hackwich
+//
+//  Created by Umesh Sarma on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Intergalactic_Travel_hackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
